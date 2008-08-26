501fcaa9b989dd46076394136a4556159a09bea2
holoed_cp <SND\holoed_cp>
2008-08-26 23:23:25 +0000

    Added LinqExpressionVisitor (Contributed by marten_range) Added Unit Tests for LinqExpressionVisitor (More tests are needed...)

---
+++ SymbolicDifferentiation/SymbolicDifferentiation/Visitors/LinqExpressionVisitor.cs
@@ -0,0 +1,95 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Authors: marten_range 
+ * 
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using SymbolicDifferentiation.AST;
+using SymbolicDifferentiation.Tokens;
+using BinaryExpression=SymbolicDifferentiation.AST.BinaryExpression;
+using Expression=System.Linq.Expressions.Expression;
+
+namespace SymbolicDifferentiation.Visitors
+{
+    using SLE = System.Linq.Expressions;
+
+    public class ToLinqExpressionVisitor : IExpressionVisitor
+    {
+        private static readonly Dictionary<Token, Func<Expression, Expression, Expression>> _token_handlers =
+            new Dictionary<Token, Func<Expression, Expression, Expression>>();
+
+        private readonly Dictionary<string, ParameterExpression> _args;
+        private readonly Stack<Expression> _stack = new Stack<Expression>();
+
+        static ToLinqExpressionVisitor()
+        {
+            _token_handlers[TokenBuilder.Symbol("+")] = Expression.Add;
+            _token_handlers[TokenBuilder.Symbol("-")] = Expression.Subtract;
+            _token_handlers[TokenBuilder.Symbol("*")] = Expression.Multiply;
+            _token_handlers[TokenBuilder.Symbol("/")] = Expression.Divide;
+            _token_handlers[TokenBuilder.Symbol("^")] = Expression.Power;
+        }
+
+        public ToLinqExpressionVisitor(IEnumerable<ParameterExpression> args)
+        {
+            _args = args.ToDictionary(x => x.Name);
+        }
+
+        public void Visit(BinaryExpression expression)
+        {
+            expression.Left.Accept(this);
+            expression.Right.Accept(this);
+
+            Expression l_right = _stack.Pop();
+            Expression l_left = _stack.Pop();
+
+            _stack.Push(_token_handlers[expression.Operator](l_left, l_right));
+        }
+
+        public void Visit(AST.Expression expression)
+        {
+            if (expression.IsNumber)
+                _stack.Push(Expression.Constant(expression.Value.Value, typeof (double)));
+            else
+                _stack.Push(_args[(string) expression.Value.Value]);
+        }
+
+        public static Expression GetExpression(
+            Type delegateType,
+            AST.Expression exr,
+            params string[] args)
+        {
+            ParameterExpression[] linqArgs = args.Select(x => Expression.Parameter(typeof (double), x)).ToArray();
+
+            var linqVisitor = new ToLinqExpressionVisitor(linqArgs);
+
+            exr.Accept(linqVisitor);
+            return Expression.Lambda(delegateType, linqVisitor._stack.Pop(), linqArgs);
+        }
+
+        public static Expression<Func<double, double>> GetExpression(
+            AST.Expression expression,
+            string arg0)
+        {
+            return (Expression<Func<double, double>>) GetExpression(
+                                                          typeof (Func<double, double>),
+                                                          expression,
+                                                          arg0);
+        }
+    }
+}
---
+++ SymbolicDifferentiation/SymbolicDifferentiationTests/LinqExpressionVisitorTests.cs
@@ -0,0 +1,144 @@
+﻿#region License
+
+/* ****************************************************************************
+ * Copyright (c) Edmondo Pentangelo. 
+ *
+ * This source code is subject to terms and conditions of the Microsoft Public License. 
+ * A copy of the license can be found in the License.html file at the root of this distribution. 
+ * By using this source code in any fashion, you are agreeing to be bound by the terms of the 
+ * Microsoft Public License.
+ *
+ * You must not remove this notice, or any other, from this software.
+ * ***************************************************************************/
+
+#endregion
+
+using System;
+using System.Linq.Expressions;
+using NUnit.Framework;
+using SymbolicDifferentiation.Extensions;
+using SymbolicDifferentiation.Tokens;
+using SymbolicDifferentiation.Visitors;
+using Expression=SymbolicDifferentiation.AST.Expression;
+
+namespace SymbolicDifferentiation.Tests
+{
+    [TestFixture]
+    public class LinqExpressionVisitorTests
+    {
+        [Test]
+        public void Costant()
+        {
+            var expression = new Expression {Value = TokenBuilder.Number(5)};
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(ConstantExpression), linqExp.Body);
+            var constant = (ConstantExpression) linqExp.Body;
+            Assert.AreEqual(ExpressionType.Constant, constant.NodeType);
+            Assert.AreEqual(5, constant.Value);
+            Assert.AreEqual(typeof(Double), constant.Type);
+        }
+
+        [Test]
+        public void Variable()
+        {
+            var expression = new Expression { Value = TokenBuilder.Variable("x") };
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(ParameterExpression), linqExp.Body);
+            var parameter = (ParameterExpression)linqExp.Body;
+            Assert.AreEqual(ExpressionType.Parameter, parameter.NodeType);
+            Assert.AreEqual("x", parameter.Name);
+            Assert.AreEqual(typeof(Double), parameter.Type);
+        }
+
+        [Test]
+        public void AddBinaryExpression()
+        {
+            var expression = new Expression {Value = TokenBuilder.Variable("x")} +
+                             new Expression {Value = TokenBuilder.Number(5)};
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(BinaryExpression), linqExp.Body);
+            var binaryExpression = (BinaryExpression)linqExp.Body;
+            AssertBinary(binaryExpression, ExpressionType.Add);
+            AssertConstant(((ConstantExpression)binaryExpression.Right), 5);
+            AssertParameter((ParameterExpression)binaryExpression.Left, "x");
+        }
+
+        [Test]
+        public void MultiplyBinaryExpression()
+        {
+            var expression = new Expression { Value = TokenBuilder.Variable("x") } *
+                             new Expression { Value = TokenBuilder.Number(5) };
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(BinaryExpression), linqExp.Body);
+            var binaryExpression = (BinaryExpression)linqExp.Body;
+            AssertBinary(binaryExpression, ExpressionType.Multiply);
+            AssertConstant(((ConstantExpression)binaryExpression.Right), 5);
+            AssertParameter((ParameterExpression)binaryExpression.Left, "x");
+        }
+
+        [Test]
+        public void PowerBinaryExpression()
+        {
+            var expression = new Expression { Value = TokenBuilder.Variable("x") } ^
+                             new Expression { Value = TokenBuilder.Number(5) };
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(BinaryExpression), linqExp.Body);
+            var binaryExpression = (BinaryExpression)linqExp.Body;
+            AssertBinary(binaryExpression, ExpressionType.Power);
+            AssertConstant(((ConstantExpression)binaryExpression.Right), 5);
+            AssertParameter((ParameterExpression)binaryExpression.Left,"x");
+        }
+
+        [Test]
+        public void AddMultiplyPrecedence()
+        {
+            var expression = new Expression { Value = TokenBuilder.Number(6) } +
+                            (new Expression { Value = TokenBuilder.Number(4) } *
+                            new Expression { Value = TokenBuilder.Number(3) });
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+            Assert.IsInstanceOfType(typeof(BinaryExpression), linqExp.Body);
+            var binaryExpression = (BinaryExpression)linqExp.Body;
+            AssertBinary(binaryExpression, ExpressionType.Add);
+            AssertConstant(((ConstantExpression)binaryExpression.Left), 6);
+            var right = (BinaryExpression) binaryExpression.Right;
+            AssertBinary(right, ExpressionType.Multiply);
+            AssertConstant(((ConstantExpression)right.Left), 4);
+            AssertConstant(((ConstantExpression)right.Right), 3);
+        }
+
+        [Test]
+        public void CSharpExpressionMatchesLinqExpressionValue()
+        {
+            var input = "2*x ^ 4 + 5*x ^ 3 + 3*x ^ 2 + 2*x + 1";
+            var expression = Tokenizer.Tokenize(input).Parse();
+            var linqExp = ToLinqExpressionVisitor.GetExpression(expression, "x");
+
+            var func = linqExp.Compile();
+
+            Func<double, double> f = x => 2 * Math.Pow(x, 4) + 5 * Math.Pow(x, 3) + 3 * Math.Pow(x, 2) + 2 * x + 1;
+
+            for (double i = 0; i < 100; i+=.1)            
+                Assert.AreEqual(f(i), func(i));
+        }
+
+        private static void AssertBinary(System.Linq.Expressions.Expression binaryExpression, ExpressionType type)
+        {
+            Assert.AreEqual(type, binaryExpression.NodeType);
+            Assert.AreEqual(typeof(Double), binaryExpression.Type);
+        }
+
+        private static void AssertParameter(ParameterExpression parameter, string name)
+        {
+            Assert.AreEqual(ExpressionType.Parameter, parameter.NodeType);
+            Assert.AreEqual(name, parameter.Name);
+            Assert.AreEqual(typeof(Double), parameter.Type);
+        }
+
+        private static void AssertConstant(ConstantExpression constant, double value)
+        {
+            Assert.AreEqual(ExpressionType.Constant, constant.NodeType);
+            Assert.AreEqual(value, constant.Value);
+            Assert.AreEqual(typeof(Double), constant.Type);
+        }
+    }
+}
