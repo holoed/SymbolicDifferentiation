f3dd1731c7df76a694f8199c279d3ebefc145a49
holoed_cp <SND\holoed_cp>
2008-09-05 08:02:13 +0000

    Refactored token type matching

--- SymbolicDifferentiation/SymbolicDifferentiationLib/FSharpLib.fs
+++ SymbolicDifferentiation/SymbolicDifferentiationLib/FSharpLib.fs
@@ -22,11 +22,14 @@ let private (|ParseRegex|_|) pattern input =
     else
         None
         
+        
+let is_a_token_of_type = fun value -> fun tokenType -> Token.Create(tokenType, value)
+        
 let private parse s =     
        match s with     
-       | ParseRegex "^[0-9]+"               result ->  Token.Create(MatchType.Number, result)
-       | ParseRegex "^[a-zA-Z]+"            result ->  Token.Create(MatchType.Variable, result)
-       | ParseRegex "^[\\^\\+\\*\\(\\)]"    result ->  Token.Create(MatchType.Symbol, result) 
+       | ParseRegex "^[0-9]+"               result ->  result |> is_a_token_of_type <|  MatchType.Number
+       | ParseRegex "^[a-zA-Z]+"            result ->  result |> is_a_token_of_type <|  MatchType.Variable
+       | ParseRegex "^[\\^\\+\\*\\(\\)]"    result ->  result |> is_a_token_of_type <|  MatchType.Symbol
        | ParseRegex "\s+"                   result ->  Token.Whitespace
        | _ -> failwith "unknown token"
   
