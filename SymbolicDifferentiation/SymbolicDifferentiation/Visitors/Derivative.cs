e813927a3534cb4c2d20d8911c439ebd33041788
holoed_cp <SND\holoed_cp>
2008-09-10 18:12:51 +0000

    Refactored C# Derivative no to use internal stack but to take advantage of the Visit generic return value.

--- SymbolicDifferentiation/SymbolicDifferentiation/Visitors/Derivative.cs
+++ SymbolicDifferentiation/SymbolicDifferentiation/Visitors/Derivative.cs
@@ -14,27 +14,36 @@
 #endregion
 
 using System;
-using System.Collections.Generic;
-using Microsoft.FSharp.Core;
 using SymbolicDifferentiation.Core.AST;
 using SymbolicDifferentiation.Core.Tokens;
 using SymbolicDifferentiation.Extensions;
 
 namespace SymbolicDifferentiation.Visitors
 {
-    public class Derivative : IExpressionVisitor<Unit>
+    public class Derivative : IExpressionVisitor<Expression>
     {
-        private readonly Stack<Expression> _stack = new Stack<Expression>();
-
-        Unit IExpressionVisitor<Unit>.Visit(BinaryExpression expression)
+        Expression IExpressionVisitor<Expression>.Visit(BinaryExpression expression)
         {
-            HandleAddition(expression);
-            HandleMultiplication(expression);
-            HandlePower(expression);
-            return default(Unit);
+            if (IsOperation(expression, "+"))
+                return Deriv(expression.Left) + Deriv(expression.Right);
+            if (IsOperation(expression, "*"))
+                return (expression.Left*Deriv(expression.Right)) + (Deriv(expression.Left)*expression.Right);
+            if (IsOperation(expression, "^"))
+                return expression.Right*(expression.Left ^ BuildExpRight(expression));
+            return default(Expression);
         }
 
-        Unit IExpressionVisitor<Unit>.Visit(Expression expression)
+        private static Expression BuildExpRight(BinaryExpression expression)
+        {
+            return new Expression { Value = (TokenBuilder.Number(Double.Parse(expression.Right.ToTokens(true).ToStringExpression()) - 1))};
+        }
+
+        private static bool IsOperation(BinaryExpression expression, string op)
+        {
+            return expression.Operator.Equals(TokenBuilder.Symbol(op));
+        }
+
+        Expression IExpressionVisitor<Expression>.Visit(Expression expression)
         {
             Token token;
             if (expression.Value.Type == MatchType.Number)
@@ -42,41 +51,15 @@ namespace SymbolicDifferentiation.Visitors
             else if (expression.Value.Type == MatchType.Variable)
                 token = TokenBuilder.Number(1);
             else
-                return default(Unit);
+                return default(Expression);
 
-            _stack.Push(new Expression {Value = token});
-            return default(Unit);
+            return new Expression {Value = token};
         }
 
         public static Expression Deriv(Expression input)
         {
             var deriv = new Derivative();
-            input.Accept(deriv);
-            return deriv._stack.Pop();
-        }
-
-        private void HandleAddition(BinaryExpression expression)
-        {
-            if (expression.Operator.Equals(TokenBuilder.Symbol("+")))
-                _stack.Push(Deriv(expression.Left) + Deriv(expression.Right));
-        }
-
-        private void HandleMultiplication(BinaryExpression expression)
-        {
-            if (expression.Operator.Equals(TokenBuilder.Symbol("*")))
-                _stack.Push((expression.Left*Deriv(expression.Right)) + (Deriv(expression.Left)*expression.Right));
-        }
-
-        private void HandlePower(BinaryExpression expression)
-        {
-            if (expression.Operator.Equals(TokenBuilder.Symbol("^")))
-                _stack.Push(expression.Right*(expression.Left ^
-                                              new Expression
-                                                  {
-                                                      Value =
-                                                          (TokenBuilder.Number(
-                                                          Double.Parse(expression.Right.ToTokens(true).ToStringExpression()) - 1))
-                                                  }));
+            return input.Accept(deriv);
         }
     }
 }
