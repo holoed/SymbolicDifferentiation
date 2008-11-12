1e5e8911bb60c18b03cf71ba1bd59cb4017a0c98
holoed_cp <SND\holoed_cp>
2008-11-12 23:36:31 +0000

    Refactored Atom Tagged Union to enforce typing constraints

--- SymbolicDifferentiation/SimbolicDifferentiation.Core/Computation/Atom.cs
+++ SymbolicDifferentiation/SimbolicDifferentiation.Core/Computation/Atom.cs
@@ -4,50 +4,68 @@ namespace SymbolicDifferentiation.Core.Computation
 {
     public class Atom
     {
-        private double _doubleValue;
-        private bool _boolValue;
+        private readonly double _doubleValue;
+        private readonly bool _boolValue;
+        private enum UnionType
+        {
+            Double, Bool
+        } UnionType _type;
 
         public Atom()
         {}
 
         public Atom(double value)
         {
+            _type = UnionType.Double;
             _doubleValue = value;
+        }
+
+        public Atom(bool value)
+        {
+            _type = UnionType.Bool;
+            _boolValue = value;
         }
 
         public static Atom operator+ (Atom left, Atom right)
         {
-            return new Atom {_doubleValue = left._doubleValue + right._doubleValue};
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue + right._doubleValue);
         }
 
         public static Atom operator -(Atom left, Atom right)
         {
-            return new Atom { _doubleValue = left._doubleValue - right._doubleValue };
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue - right._doubleValue);
         }
 
         public static Atom operator *(Atom left, Atom right)
         {
-            return new Atom { _doubleValue = left._doubleValue * right._doubleValue };
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue * right._doubleValue);
         }
 
         public static Atom operator /(Atom left, Atom right)
         {
-            return new Atom { _doubleValue = left._doubleValue / right._doubleValue };
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue / right._doubleValue);
         }
 
         public static Atom operator >(Atom left, Atom right)
         {
-            return new Atom { _boolValue = left._doubleValue > right._doubleValue };
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue > right._doubleValue);
         }
 
         public static Atom operator <(Atom left, Atom right)
         {
-            return new Atom { _boolValue = left._doubleValue < right._doubleValue };
+            EnsureTypeConsistency(left, right);
+            return new Atom(left._doubleValue < right._doubleValue);
         }
 
         public static Atom operator ^(Atom left, Atom right)
         {
-            return new Atom {_doubleValue = Math.Pow(left._doubleValue, right._doubleValue) };
+            EnsureTypeConsistency(left, right);
+            return new Atom(Math.Pow(left._doubleValue, right._doubleValue));
         }
 
         public static implicit operator bool(Atom value)
@@ -62,12 +80,12 @@ namespace SymbolicDifferentiation.Core.Computation
 
         public static implicit operator Atom(double value)
         {
-            return new Atom {_doubleValue = value};
+            return new Atom(value);
         }
 
         public static implicit operator Atom(bool value)
         {
-            return new Atom { _boolValue = value };
+            return new Atom(value);
         }
 
         public override bool Equals(object obj)
@@ -85,7 +103,13 @@ namespace SymbolicDifferentiation.Core.Computation
 
         public override int GetHashCode()
         {
-            return _doubleValue.GetHashCode();
+            return _doubleValue.GetHashCode() ^ _boolValue.GetHashCode();
+        }
+
+        private static void EnsureTypeConsistency(Atom left, Atom right)
+        {
+            if (left._type != right._type)
+                throw new InvalidCastException(String.Format("{0} and {1} types are not compatible", left._type, right._type));
         }
     }
 }
